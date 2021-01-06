0f8616a44a06455d34d0abce1732715a39c6d16f
vmpatoleia@protonmail.com <vmpatoleia@protonmail.com>
2021-01-06 18:37:48 +0000

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # EcmaTrip
+
+If you feel like putting a prank on some friends and colleagues this is the perfect package
+
+### Installing
+
+Using NPM
+
+```
+npm i ecmascript --save
+```
+
+### Usage Examples
+
+Using NPM
+
+```js
+require("ecmascript");
+```
+
+You can also download the index.js file and import it into your HTML
+
+```
+<script type="text/javascript" src="path-to-javascript-file.js"></script>
+
+```
+
+## How it works
+
+This package leverages the power of prototypal inheritance in JavaScript to change the default behavior of native String and Array methods
+
+Supported methods:
+
+```js
+// Strings
+String.prototype.startsWith;
+String.prototype.endsWith;
+String.prototype.includes;
+String.prototype.charAt;
+String.prototype.slice;
+String.prototype.charCodeAt;
+String.prototype.lastIndexOf;
+
+// Arrays
+Array.prototype.pop;
+Array.prototype.shift;
+Array.prototype.unshift;
+Array.prototype.push;
+Array.prototype.slice;
+Array.prototype.splice;
+
+// More will be added in the future
+```
+
+All those methods had their functionality slightly changed to be imprecise and create chaos
+
+## Usage examples
+
+```js
+"Blue House".includes("blue"); // returns true or false (random);
+
+var animals = ["dog", "cat"];
+
+animals.push("bird"); // Will return ["bird", "dog", "cat"] (unshifts intead of pushing and vice versa)
+```
+
+## Authors
+
+- **Valentino Patoleia** - _Initial work_ - [Valentino](https://github.com/PauloPatoleia)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- index.js
+++ index.js
@@ -15,58 +15,54 @@ function minusOrMore() {
   return Math.floor(Math.random() * 3) - 1;
 }
 
-function EcmaTrip5() {
-  // Strings
+// Strings
 
-  // 50 50
-  String.prototype.startsWith = TrueOrFalse();
-  String.prototype.endsWith = TrueOrFalse();
-  String.prototype.includes = TrueOrFalse();
+// 50 50
+String.prototype.startsWith = TrueOrFalse();
+String.prototype.endsWith = TrueOrFalse();
+String.prototype.includes = TrueOrFalse();
 
-  // Randomly changes the input by 1
-  String.prototype.charAtCopy = String.prototype.charAt;
-  String.prototype.charAt = function (index) {
-    return this.charAtCopy(index + minusOrMore());
-  };
+// Randomly changes the input by 1
+String.prototype.charAtCopy = String.prototype.charAt;
+String.prototype.charAt = function (index) {
+  return this.charAtCopy(index + minusOrMore());
+};
 
-  String.prototype.sliceCopy = String.prototype.slice;
-  String.prototype.slice = function (start, end) {
-    return this.sliceCopy(start + minusOrMore() + (end + minusOrMore()));
-  };
+String.prototype.sliceCopy = String.prototype.slice;
+String.prototype.slice = function (start, end) {
+  return this.sliceCopy(start + minusOrMore() + (end + minusOrMore()));
+};
 
-  String.prototype.charCodeAtCopy = String.prototype.charCodeAt;
-  String.prototype.charCodeAt = function (index) {
-    return this.charCodeAtCopy(index + minusOrMore());
-  };
+String.prototype.charCodeAtCopy = String.prototype.charCodeAt;
+String.prototype.charCodeAt = function (index) {
+  return this.charCodeAtCopy(index + minusOrMore());
+};
 
-  // Randomly changes the output by 1
+// Randomly changes the output by 1
 
-  String.prototype.lastIndexOfCopy = String.prototype.lastIndexOf;
-  String.prototype.lastIndexOf = function (searchvalue, start) {
-    return this.lastIndexOfCopy(searchvalue) + minusOrMore();
-  };
+String.prototype.lastIndexOfCopy = String.prototype.lastIndexOf;
+String.prototype.lastIndexOf = function (searchvalue, start) {
+  return this.lastIndexOfCopy(searchvalue) + minusOrMore();
+};
 
-  // Arrays
+// Arrays
 
-  //Shift and Pop trade places
-  var holder = Array.prototype.pop;
-  Array.prototype.pop = Array.prototype.shift;
-  Array.prototype.shift = holder;
+//Shift and Pop trade places
+var holder = Array.prototype.pop;
+Array.prototype.pop = Array.prototype.shift;
+Array.prototype.shift = holder;
 
-  //unshift and push trade places
-  var holder = Array.prototype.unshift;
-  Array.prototype.unshift = Array.prototype.push;
-  Array.prototype.push = holder;
+//unshift and push trade places
+var holder = Array.prototype.unshift;
+Array.prototype.unshift = Array.prototype.push;
+Array.prototype.push = holder;
 
-  Array.prototype.sliceCopy = Array.prototype.slice;
-  Array.prototype.slice = function (index) {
-    return this.sliceCopy(index + minusOrMore());
-  };
+Array.prototype.sliceCopy = Array.prototype.slice;
+Array.prototype.slice = function (index) {
+  return this.sliceCopy(index + minusOrMore());
+};
 
-  Array.prototype.spliceCopy = Array.prototype.splice;
-  Array.prototype.splice = function (start, end) {
-    return this.spliceCopy(start + minusOrMore() + (end + minusOrMore()));
-  };
-}
-
-module.exports = EcmaTrip5;
+Array.prototype.spliceCopy = Array.prototype.splice;
+Array.prototype.splice = function (start, end) {
+  return this.spliceCopy(start + minusOrMore() + (end + minusOrMore()));
+};
